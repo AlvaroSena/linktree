d3704468b4b0d48619a98f8c14ca43961a05e033
alvarosena <alvarosenacs.c@gmail.com>
2023-09-21 16:10:30 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,49 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import './page.modules.scss'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
+    <div className="container">
+      <div className="content">
+        <div className="profile">
+          <img
+            src="https://avatars.githubusercontent.com/u/73470355?s=400&u=f3ca3fe115cdeac57075dea21422684a25238f59&v=4"
+            alt=""
+            className="avatar"
+          />
+          <span className="username">Álvaro Sena</span>
+        </div>
+
+        <div className="list">
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="item"
             target="_blank"
             rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/alvarosen4/"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            Veja o meu Linkedin
+          </a>
+          <a
+            className="item"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/alvarosena"
+          >
+            Conheça o meu GitHub
+          </a>
+          <a
+            className="item"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="NewCV.pdf"
+          >
+            Ver CV
           </a>
         </div>
+
+        <footer>
+          <p>Website desenvolvido por Álvaro Sena 🤌</p>
+        </footer>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
